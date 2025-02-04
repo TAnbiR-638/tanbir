0156463902da60cf24d0257930d25fc7d8d50e2b
Md. Tanbirul Islam <105564476+TAnbiR-638@users.noreply.github.com>
2025-02-04 11:51:58 +0600

    Add files via upload

---
+++ tanbir.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+name = "Md. Tanbirul Islam"
+roll = "B190305019"
+date = datetime.today().strftime('%Y-%m-%d')
+
+print("Name:", name)
+print("Roll:", roll)
+print("Date:", date)
+
